fb20dca6034dec1f40483a8303cb51ef736d5249
TemptingTaco12 <albirchler@hotmail.com>
2023-06-29 14:14:55 +0200

    Optimized the create schema script

--- create_schema.py
+++ create_schema.py
@@ -4,35 +4,36 @@ import csv
 import re
 import time
 
-time.sleep(10)
+time.sleep(30)
 
 # Neo4j connection details
 uri = "bolt://malware-db:7687"
+#uri = "bolt://localhost:7687"
 # Connect to Neo4j
 driver = GraphDatabase.driver(uri)
 
 # Function to create a Malware type node
 def create_malware_type_node(tx, malware_type):
     query = '''
-    MATCH (node1:Malware_Type), (node2:Malware)
-    WHERE node1.name = $malware_type
+    CREATE (node1:Malware_Type {name: $malware_type})
+    WITH node1
+    MATCH (node2:Malware)
     CREATE (node1)-[r:IS_A]->(node2)
     '''
-    
-    tx.run("CREATE (:Malware_Type {name: $malware_type});", malware_type=malware_type)
+
     tx.run(query, malware_type=malware_type)
     
 # Function to create a Malware instance node
 def create_malware_instance_node(tx, malware_instance, malware_type):
     query = '''
-    MATCH (node1:Malware_Instance), (node2:Malware_Type)
-    WHERE node1.name = $malware_instance AND node2.name = $malware_type
+    CREATE (node1:Malware_Instance {name: $malware_instance})
+    WITH node1
+    MATCH (node2:Malware_Type)
+    WHERE node2.name = $malware_type
     CREATE (node1)-[r:IS_A]->(node2)
     '''
     
-    tx.run("CREATE (:Malware_Instance {name: $malware_instance})", malware_instance=malware_instance)
     tx.run(query, malware_instance=malware_instance, malware_type=malware_type)
-
 
 def parse_hash(file):
     start_index = file.rfind("-") + 1
@@ -44,12 +45,13 @@ def parse_hash(file):
 # Function to create a Sample node
 def create_sample_node(tx, hash, malware_instance):
     query = '''
-    MATCH (node1:Sample), (node2:Malware_Instance)
-    WHERE node1.hash = $hash AND node2.name = $malware_instance
+    CREATE (node1:Sample {hash: $hash})
+    WITH node1
+    MATCH (node2:Malware_Instance)
+    WHERE node2.name = $malware_instance
     CREATE (node1)-[r:IS_A]->(node2)
     '''
-    
-    tx.run("CREATE (:Sample {hash: $hash})", hash=hash)
+
     tx.run(query, hash=hash, malware_instance=malware_instance)
 
     return hash
@@ -69,7 +71,7 @@ def is_float(s):
         return False
 
 def create_process_nodes(tx, csv_file, hash):
-    print(csv_file)
+#    print(csv_file)
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
 
@@ -104,8 +106,8 @@ WITH row WHERE '''
                 query += " AND "
 
     query += '''
-MERGE (n:Process {'''
-    
+CREATE (n:Process {'''
+
     for idx, header in enumerate(headers):
         if idx < len(headers) - 1:
             query += headers_cleaned[idx]
@@ -115,34 +117,15 @@ MERGE (n:Process {'''
             if idx < len(headers) - 2:
                 query += ", "
     
-    query += '''})
-ON CREATE
-    SET '''
-
-    for idx, header in enumerate(headers):
-        if idx < len(headers) - 1:
-            query += "n."
-            query += headers_cleaned[idx]
-            query += " = "
-            if data_types[idx] == "int":
-                query += "toInteger("
-            elif data_types[idx] == "float":
-                query += "toFloat("
-            query += "row.`"
-            query += header
-            query += "`"
-            if data_types[idx] == "int" or data_types[idx] == "float":
-                query += ")"
-            if idx < len(headers) - 2:
-                query += ", "
-
-    query += '''
-    WITH n
-    MATCH (sampleNode:Sample {hash: $hash})
-    MERGE (sampleNode)-[:PERFORMED]->(n)
-    '''
+    query += "})"
     
-    #print(query)
+    query += '''
+WITH n
+MATCH (sampleNode:Sample {hash: $hash})
+CREATE (sampleNode)-[:PERFORMED]->(n)
+'''
+    
+#    print(query)
     tx.run(query, hash=hash)            
 
 # Folder path
